9c066df5430a22074ebb9ef44aa6a0b765e9936a
Vanshika-2609 <rajvanshika2609@gmail.com>
2025-02-17 15:32:58 +0530

    read and write file

---
+++ Day1/p8.js
@@ -0,0 +1,8 @@
+const fs= require('fs');
+//const data ="I am appended data"
+
+fs.appendFile("./data.txt","I am appended data", (err)=>{
+    if (err)
+        console.log(err);
+    console.log("data appended");
+});
