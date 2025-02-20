e49f5aeb60c4e537dbe9cefa9793f01c4a31e897
Vanshika-2609 <rajvanshika2609@gmail.com>
2025-02-20 14:52:15 +0530

    Day1 fs promises

---
+++ Day1/p11.js
@@ -0,0 +1,17 @@
+const fs =require('fs');
+
+const write=(data)=>{
+    
+    fs.writeFile("./mydir/data.txt",data , (err)=>{
+        if (err)
+            console.log("error writing file",err);
+        else
+            console.log("file written successfully");
+    })
+
+}
+
+
+
+write("i am new data");
+
---
+++ Day1/p13.js
@@ -0,0 +1,7 @@
+// const fs= require('fs');
+
+// fs.rmdir("./mydir",(err)=>{
+//     if (err)
+//         console.log("err",err)
+//     console.log("directory deleted successfully");
+// })
